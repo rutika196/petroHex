8110f1e8078e80d5f10d8720c2dcc754ccc3ae21
Rutika Patel <rutikap0619@gmail.com>
2025-04-03 15:04:44 -0400

    Remove unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import { generateChatResponse, formatMessagesForOpenAI } from './services/openaiService'
 
